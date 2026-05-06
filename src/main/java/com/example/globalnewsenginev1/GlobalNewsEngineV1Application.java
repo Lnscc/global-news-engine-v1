4924ba0dbdd1d8c6ffd1fb1732892676fc15c512
Linus Musshafen <linus.musshafen@gmail.com>
2026-05-06 18:54:00 +0200

    Initialize Maven Spring Boot project with PostgreSQL integration and Docker Compose setup

---
+++ src/main/java/com/example/globalnewsenginev1/GlobalNewsEngineV1Application.java
@@ -0,0 +1,13 @@
+package com.example.globalnewsenginev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalNewsEngineV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalNewsEngineV1Application.class, args);
+    }
+
+}
